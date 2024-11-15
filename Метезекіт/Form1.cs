8c1e842a3be62f23f5caf9b0f2173c0f6c69f6d3
Rmoman-Kyprienov <jxdjdjxjdn@gmail.com>
2024-11-15 20:40:53 +0200

    code changes

--- Метезекіт/Form1.cs
+++ Метезекіт/Form1.cs
@@ -1,20 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace Mtzkit
+namespace HelloWorld
 {
-    public partial class Form1 : Form
+    class Program
     {
-        public Form1()
+        static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello, World!");
         }
     }
 }
